063eb17c385dcf4c09dd1c6efbeac533127ec6f5
juhahinkula <hinkula@gmail.com>
2019-04-11 19:03:28 +0300

    chapter 10 added

---
+++ Chapter10/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AppBar position="static" color="default">
+            <Toolbar>
+              <Typography variant="h6" color="inherit">
+                CarList
+              </Typography>
+            </Toolbar>
+          </AppBar>
+      </div>
+    );
+  }
+}
+
+export default App;
