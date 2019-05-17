dc20c15dd13d3e51046f6d6ca807b66b94ff6fc5
juhahinkula <hinkula@gmail.com>
2019-05-17 20:19:59 +0300

    code updated

--- Chapter14/src/App.js
+++ Chapter14/src/App.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Login from './components/Login';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <AppBar position="static" color="default">
-            <Toolbar>
-              <Typography variant="h6" color="inherit">
-                CarList
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <Login />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            CarList
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Login />
+    </div>
+  );
 }
 
 export default App;
--- Chapter14/src/App.test.js
+++ Chapter14/src/App.test.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import TestRenderer from 'react-test-renderer';
-import AddCar from './components/AddCar';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
-});
-
-it('renders a snapshot', () => {
-  const tree = TestRenderer.create(<AddCar/>).toJSON();
-  expect(tree).toMatchSnapshot();
 });
