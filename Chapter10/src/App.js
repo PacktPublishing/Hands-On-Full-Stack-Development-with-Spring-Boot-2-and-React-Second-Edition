7cbb96db1a98310539a1b8a3a604d236ecbce702
juhahinkula <hinkula@gmail.com>
2019-05-18 18:03:50 +0300

    code updated

--- Chapter10/src/App.js
+++ Chapter10/src/App.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <AppBar position="static" color="default">
-            <Toolbar>
-              <Typography variant="h6" color="inherit">
-                CarList
-              </Typography>
-            </Toolbar>
-          </AppBar>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            CarList
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
 export default App;
