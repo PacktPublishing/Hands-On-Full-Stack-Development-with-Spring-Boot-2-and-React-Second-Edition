f158a4cec6934b7ae88df6725d1de40ea7789fa2
juhahinkula <hinkula@gmail.com>
2019-05-18 18:23:35 +0300

    code updated

--- Chapter13/src/App.test.js
+++ Chapter13/src/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import TestRenderer from 'react-test-renderer';
 import AddCar from './components/AddCar';
+import renderer from 'react-test-renderer'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -11,6 +11,6 @@ it('renders without crashing', () => {
 });
 
 it('renders a snapshot', () => {
-  const tree = TestRenderer.create(<AddCar/>).toJSON();
+  const tree = renderer.create(<AddCar/>).toJSON();
   expect(tree).toMatchSnapshot();
 });
