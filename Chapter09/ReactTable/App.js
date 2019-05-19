e45e4bd0bcbc73b8d81caa45f30f1d217e9d6f9d
juhahinkula <hinkula@gmail.com>
2019-05-19 11:24:20 +0300

    code updated

--- Chapter09/ReactTable/App.js
+++ Chapter09/ReactTable/App.js
@@ -1,63 +1,54 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import ReactTable from "react-table";
 import 'react-table/react-table.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { keyword: '', data: [] };
-  }
+function App() {
+  const [data, setData] = useState([]);
+  const [keyword, setKeyword] = useState('');
 
-  fetchData = () => {
-    const url = `https://api.github.com/search/repositories?q=${this.state.keyword}`;
+  const fetchData = () => {
+    const url = `https://api.github.com/search/repositories?q=${keyword}`;
     fetch(url)
     .then(response => response.json()) 
     .then(responseData => {
-      this.setState({data : responseData.items }); 
-    });  
+      setData(responseData.items); 
+    }); 
   }
   
-  handleChange = (e) => {
-    this.setState({keyword: e.target.value});
-  }
+  const handleChange = (e) => {
+    setKeyword(e.target.value);
+  }  
 
-  btnClick = (value) => {
+  const btnClick = (value) => {
     alert(value);
   }
 
-  render() {
-    const columns = [{
-      Header: 'Name',       // Header of the column
-      accessor: 'full_name' // Value accessor
-    }, {
-      Header: 'URL',
-      accessor: 'html_url',
-    }, {
-      Header: 'Owner',
-      accessor: 'owner.login',
-    }, {
-      id: 'button',
-      sortable: false,
-      filterable: false,
-      width: 100,
-      accessor: 'full_name',
-      Cell: ({value}) => (<button onClick={()=>{this.btnClick(value)}}>Press me</button>)
-    }]
+  const columns = [{
+    Header: 'Name', // Header of the column
+    accessor: 'full_name' // Value accessor
+  }, {
+    Header: 'URL',
+    accessor: 'html_url',
+  }, {
+    Header: 'Owner',
+    accessor: 'owner.login',
+  }, {
+    id: 'button',
+    sortable: false,
+    filterable: false,
+    width: 100,
+    accessor: 'full_name',
+    Cell: ({value}) => (<button onClick={() => {btnClick(value)}}>Press me</button>)
+}]
 
-    return (
-      <div className="App">
-        <input type="text" onChange={this.handleChange} />
-        <button onClick={this.fetchData} value={this.state.keyword} >Fetch</button>
-        <ReactTable
-          data={this.state.data}
-          columns={columns}
-          filterable={true}
-          defaultPageSize = {10}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <input type="text" onChange={handleChange} />
+      <button onClick={fetchData} value={keyword} >fetch</button>
+      <ReactTable data={data} columns={columns} filterable={true} defaultPageSize = {10} />
+    </div>
+  );
 }
 
 export default App;
